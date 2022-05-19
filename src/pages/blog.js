2a4c23a4da80ef383ccc3d7d7922dad02af66967
dyx9 <747736317@qq.com>
2022-05-19 16:29:03 +0800

    Finish Tutorial 4, pull data into a page component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,34 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return(
         <Layout pageTitle="My Blog Page">
-            <p>This is the blog page</p>
+            <ul>
+
+            {
+                data.allFile.nodes.map(node =>
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                )
+            }
+
+            </ul>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query {
+        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+            nodes {
+                name
+            }
+        }
+    }
+`
+
 
 export default BlogPage
