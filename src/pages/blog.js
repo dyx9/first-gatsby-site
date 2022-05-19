7cf4d53fc322573e8031658318c56f0acba04fd0
dyx9 <747736317@qq.com>
2022-05-19 13:19:16 +0800

    Part of Tutorial 4: GraphQL

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+    return(
+        <Layout pageTitle="My Blog Page">
+            <p>This is the blog page</p>
+        </Layout>
+    )
+}
+
+export default BlogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        src="https://www.apple.com.cn/iphone-13-pro/images/meta/wechat/iphone-13-pro_overview__b1vk24cp916q_og.png"
       />
     </Layout>
   )
