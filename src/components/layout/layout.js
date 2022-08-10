57a33103da00e2224db1a0cbcadb69809aa21874
dyx9 <747736317@qq.com>
2022-08-10 15:39:36 +0800

    Include mobile header in sidebar

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,31 +1,10 @@
 import * as React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import { useRef, useState, } from 'react';
 import Sidebar from '../sidebar/sidebar';
 import * as styles from './layout.module.scss';
 
 
 const Layout = ({ pageTitle, children }) => {
-
-  // const sidebarRef = useRef(null)
-  // const contentRef = useRef(null)
-  // const [openSidebar, setOpenSidebar] = useState(false)
-
-  // const onToggle = () => {
-  //   setOpenSidebar(pre => !pre)
-  //   updateClassName()
-  // }
-
-  // const updateClassName = () => {
-  //   if (sidebarRef.current.className == "sidebar") {
-  //     sidebarRef.current.className = 'sidebar active'
-  //     contentRef.current.className = 'content push'
-  //   }
-  //   else{
-  //     sidebarRef.current.className = 'sidebar'
-  //     contentRef.current.className = 'content'
-  //   }
-  // }
 
   const data = useStaticQuery(graphql`
     query {
@@ -39,12 +18,8 @@ const Layout = ({ pageTitle, children }) => {
 
   return (
     <div className={styles.container}>
-      <Sidebar
-        // onToggle={onToggle}
-        // openSidebar={openSidebar}
-        // sidebarRef={sidebarRef}
-      />
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <Sidebar/>
       {/* <header className={siteTitle}>{data.site.siteMetadata.title}</header> */}
       <main className={styles.content}>
         {/* <h1 className='heading'>{pageTitle}</h1> */}
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,67 +1,60 @@
 import * as React from 'react'
 import { useState, useRef } from 'react'
-import MobileNavbar from '../mobile-navbar/mobile-navbar'
+import { MdClose, MdMenu } from 'react-icons/md';
 import * as styles from '../sidebar/sidebar.module.scss'
 
 const Sidebar = () => {
 
-    const [open, setOpen] = useState(false)
-    const sidebarRef = useRef(null)
+  const [open, setOpen] = useState(false)
+  const sidebarRef = useRef(null)
 
-    const clickHandler = () => {
-        setOpen(!open)
-        updateClassName()
-    }
+  const clickHandler = () => {
+    setOpen(!open)
+    updateClassName()
+  }
 
-    const updateClassName = () => {
-        console.log(styles.sidebar);
-        console.log(sidebarRef);
-        if (sidebarRef.current.className === styles.sidebar) {
-            sidebarRef.current.className += " " + styles.active;
-        }
-        else {
-            sidebarRef.current.className = styles.sidebar;
-        }
-        
-        console.log(sidebarRef);
+  const updateClassName = () => {
+    if (sidebarRef.current.className === styles.sidebar) {
+      sidebarRef.current.className += " " + styles.active;
     }
+    else {
+      sidebarRef.current.className = styles.sidebar;
+    }
+  }
 
   return (
     <>
-        {/* <div className={styles.sidebar + (open ? " " + styles.active : "")} > */}
-        <div className={styles.sidebar} ref={sidebarRef}>
-            <MobileNavbar onToggle={clickHandler} open={open}/>
-            <ul className={styles.menu}>
-                <li className={styles.logo}>
-                    <a href="/">Yixuan's Website</a>
-                </li>
+      {/* <div className={styles.sidebar + (open ? " " + styles.active : "")} > */}
+      <div className={styles.sidebar} ref={sidebarRef}>
 
-                <li className='item'>
-                    <a href="/about">About</a>
-                </li>
+        {/* mobile header */}
+        <ul className={styles.mobileHeader}>
+              <li onClick={clickHandler}>
+                {!open &&<a><MdMenu /></a>}
+                {open &&<a><MdClose /></a>}
+              </li>
+              <li className={styles.logo}> <a href="/">Yixuan's Website</a> </li>
+        </ul>
 
-                <li className='item'>
-                    <a href="/blog">Blog</a>
-                </li>
+        <ul className={styles.menu}>
+          <li className={styles.logo}> <a href="/">Yixuan's Website</a> </li>
+          <li> <a href="/about">About</a> </li>
+          <li> <a href="/blog">Blog</a> </li>
+          <li> <a hrefo="/">Contact</a> </li>
+        </ul>
 
-                <li className='item'>
-                    <a hrefo="/">Contact</a>
-                </li>
-            </ul>
+        <ul className={styles.interactiveArea1}>
+          <li> <a href="#">interactive</a> </li>
+          <li> <a href="#">interactive</a> </li>
+          <li> <a href="#">interactive</a> </li>
+        </ul>
 
-            <ul className={styles.interactiveArea1}>
-                <li className="item"><a href="#">interactive</a></li>
-                <li className="item"><a href="#">interactive</a></li>
-                <li className="item"><a href="#">interactive</a></li>
-            </ul>
-
-            <ul className={styles.sidebarFooter}>
-                <li className="item"><a href="#">footer</a></li>
-                <li className="item"><a href="#">footer</a></li>
-            </ul>
-        </div>
+        <ul className={styles.sidebarFooter}>
+          <li> <a href="#">footer</a> </li>
+          <li> <a href="#">footer</a> </li>
+        </ul>
+      </div>
     </>
-
   )
 }
 
