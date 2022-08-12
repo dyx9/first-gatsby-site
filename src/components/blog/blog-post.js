37c86a511fccd4b8679d028f330a84e1202958d1
dyx9 <747736317@qq.com>
2022-08-12 15:32:05 +0800

    rough theme for post

--- src/components/blog/blog-post.js
+++ src/components/blog/blog-post.js
@@ -2,30 +2,38 @@ import * as React from 'react'
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import * as styles from './blog-post.module.scss';
-
 import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
 
 
 const BlogPost = ({ frontmatter, body }) => {
 
+
   return (
     <>
-      <p>{frontmatter.date}</p>
+
       <GatsbyImage
+        className={styles.heroImage}
         image={getImage(frontmatter.hero_image)}
         alt={frontmatter.hero_image_alt}
       />
-      
-      <p>
+      <p className={styles.credit}>
         Photo Credit:{" "}
         <a href={frontmatter.hero_image_credit_link}>
           {frontmatter.hero_image_credit_text}
         </a>
       </p>
 
-      <MDXRenderer>
-        {body}
-      </MDXRenderer>
+      <p className={styles.title}>{frontmatter.title}</p>
+      <p className={styles.date}>{frontmatter.date}</p>
+
+
+
+      <div className={styles.MDXBody}>
+        <MDXRenderer>
+          {body}
+        </MDXRenderer>
+      </div>
+
 
     </>
   )
