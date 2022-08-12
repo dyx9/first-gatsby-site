56b703ea379bfd614d65cc85d44e3940b113ee4e
dyx9 <747736317@qq.com>
2022-08-12 17:29:16 +0800

    minor css change

--- src/components/blog/blog-post.js
+++ src/components/blog/blog-post.js
@@ -3,30 +3,23 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import * as styles from './blog-post.module.scss';
 import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
+import { Link } from 'gatsby';
 
 
 const BlogPost = ({ frontmatter, body }) => {
 
-
   return (
     <>
-
-      <GatsbyImage
-        className={styles.heroImage}
-        image={getImage(frontmatter.hero_image)}
-        alt={frontmatter.hero_image_alt}
-      />
-      <p className={styles.credit}>
-        Photo Credit:{" "}
-        <a href={frontmatter.hero_image_credit_link}>
-          {frontmatter.hero_image_credit_text}
-        </a>
-      </p>
+      <Link to={frontmatter.hero_image_credit_link}>
+        <GatsbyImage
+          className={styles.heroImage}
+          image={getImage(frontmatter.hero_image)}
+          alt={frontmatter.hero_image_alt}
+        />
+      </Link>
 
       <p className={styles.title}>{frontmatter.title}</p>
       <p className={styles.date}>{frontmatter.date}</p>
-
-
 
       <div className={styles.MDXBody}>
         <MDXRenderer>
