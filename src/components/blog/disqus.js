cdb804f93a4253d7767354bd4aa8d6c0f3243899
dyx9 <747736317@qq.com>
2023-04-06 15:48:02 +0100

    style comment

---
+++ src/components/blog/disqus.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Disqus } from 'gatsby-plugin-disqus';
+import * as styles from './disqus.module.scss';
+
+const MyDisqus = ({ pageContext, frontmatter }) => {
+
+    const disqusConfig = {
+        url: `https://dyx.gatsbyjs.io/blog/${pageContext.slug}`,
+        identifier: pageContext.slug,
+        title: frontmatter.title,
+    }
+
+    return (
+        <div className={styles.commentContainer}>
+            <Disqus config={disqusConfig} />
+        </div>
+
+    )
+    
+}
+
+export default MyDisqus
+
