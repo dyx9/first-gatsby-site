f10678f403ce8dab555a5c9cb1bab7e99c4385ad
dyx9 <747736317@qq.com>
2023-12-23 13:29:35 +0000

    Navigate Section for Mobile - add an addition navigate section for mobile layout, reduce the number of clicks of switching between different pages - recreat tag-list pages for mobile layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import * as styles from './layout.module.scss';
+import MobileNav from '../mobile-nav/mobile-nav';
 
 
 const Layout = ({ pageTitle, children }) => {
@@ -18,6 +19,9 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <div className={styles.container}>
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <div className={styles.mobileNav}>
+        <MobileNav />
+      </div>
       {children}
     </div>
   )
---
+++ src/components/mobile-nav/mobile-nav.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import * as styles from './mobile-nav.module.scss';
+import { useLocation } from "@reach/router";
+
+
+const MobileNav = () => {
+
+    const location = useLocation();
+    const pathSegments = location.pathname.split('/').filter(Boolean);
+
+    const parentPaths = [{ name: 'Home', path: '/' }];
+  
+    pathSegments.forEach((segment, index) => {
+        const pathToSegment = `/${pathSegments.slice(0, index + 1).join('/')}`;
+        parentPaths.push({ name: segment, path: pathToSegment });
+    });
+    
+    if (location.pathname !== '/')
+    return (
+        <div className={styles.mobileNavContainer}>
+        {parentPaths.map((parent, index) => (
+            <span key={index}>
+                {index > 0 && ' > '}
+                <Link to={parent.path}> {parent.name}</Link>
+            </span>
+        ))}
+        </div>
+    )
+    else return null;
+}
+
+export default MobileNav
