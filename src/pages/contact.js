4dab9181dba00e4a8605dd68fa49c02e1edc10e1
dyx9 <747736317@qq.com>
2023-12-27 17:09:10 +0000

    Upload more posts, and a simple contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout/layout'
 import Sidebar from '../components/sidebar/sidebar'
 import Main from '../components/layout/main'
+import * as styles from './contact.module.scss';
 
 
 const ContactPage = () => {
@@ -9,7 +10,10 @@ const ContactPage = () => {
     <Layout pageTitle="Contact Me">
       <Sidebar />
       <Main>
-        <p>Hi, this is the contact page</p>
+        <div className={styles.contactContainer}>
+            <p id={styles.lineOne}>Let's Talk</p>
+            <p>I plan to build a form that you can send a message to me directly from this page, but for now, feel free to use social media links from the sidebar (or the drop-down menu if you're visiting this page from a mobile device) </p>
+          </div>
       </Main>
     </Layout>
   )
